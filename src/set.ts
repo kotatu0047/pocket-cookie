1bb0cac601e01772be56e72592ba838b72be5985
yamamichi <yamamichi1995@outlook.jp>
2019-11-25 20:44:46 +0900

    refactoring set.ts

--- src/set.ts
+++ src/set.ts
@@ -23,7 +23,49 @@ export interface CookieAttributes {
   readonly samesite?: 'strict' | 'lax'
 }
 
-// TODO percent encode autoCast  refactoring
+const createStringAttribute = (attributes: CookieAttributes) => {
+  let stringAttributes = ''
+  let expires = ''
+  if (attributes.expires !== undefined) {
+    const expiresDate =
+      attributes.expires instanceof Date
+        ? attributes.expires
+        : new Date(Date.now() + attributes.expires * millisecondsOfOneDay)
+    expires = expiresDate.toUTCString()
+  }
+  const path = attributes.path !== undefined ? attributes.path : ''
+  const domain = attributes.domain !== undefined ? attributes.domain : ''
+  const isSecure = attributes.secure !== undefined ? attributes.secure : false
+  const maxAge =
+    attributes.maxAge !== undefined
+      ? `${attributes.maxAge * secondsOfOneDay}`
+      : ''
+  const samesite = attributes.samesite !== undefined ? attributes.samesite : ''
+
+  stringAttributes +=
+    expires || path || domain || isSecure || maxAge || samesite ? '; ' : ''
+  stringAttributes += expires
+    ? `${nameof<CookieAttributes>(o => o.expires)}=${expires}; `
+    : ''
+  stringAttributes += path
+    ? `${nameof<CookieAttributes>(o => o.path)}=${removeSemicolon(path)}; `
+    : ''
+  stringAttributes += domain
+    ? `${nameof<CookieAttributes>(o => o.domain)}=${removeSemicolon(domain)}; `
+    : ''
+  stringAttributes += isSecure
+    ? `${nameof<CookieAttributes>(o => o.secure)}; `
+    : ''
+  // cannot naming CookieAttributes.max-age
+  stringAttributes += maxAge ? `${'max-age' + '='}${maxAge}; ` : ''
+  stringAttributes += samesite
+    ? `${nameof<CookieAttributes>(o => o.samesite)}=${samesite}; `
+    : ''
+
+  return stringAttributes
+}
+
+// TODO percent encode autoCast
 const set = <T1, T2 extends object>(
   key: string,
   value: autoCast<T1, T2>,
@@ -34,47 +76,8 @@ const set = <T1, T2 extends object>(
   }
 
   let stringAttributes = ''
-  if (attributes !== undefined) {
-    let expires = ''
-    if (attributes.expires !== undefined) {
-      const expiresDate =
-        attributes.expires instanceof Date
-          ? attributes.expires
-          : new Date(Date.now() + attributes.expires * millisecondsOfOneDay)
-      expires = expiresDate.toUTCString()
-    }
-    const path = attributes.path !== undefined ? attributes.path : ''
-    const domain = attributes.domain !== undefined ? attributes.domain : ''
-    const isSecure = attributes.secure !== undefined ? attributes.secure : false
-    const maxAge =
-      attributes.maxAge !== undefined
-        ? `${attributes.maxAge * secondsOfOneDay}`
-        : ''
-    const samesite =
-      attributes.samesite !== undefined ? attributes.samesite : ''
-
-    stringAttributes +=
-      expires || path || domain || isSecure || maxAge || samesite ? '; ' : ''
-    stringAttributes += expires
-      ? `${nameof<CookieAttributes>(o => o.expires)}=${expires}; `
-      : ''
-    stringAttributes += path
-      ? `${nameof<CookieAttributes>(o => o.path)}=${removeSemicolon(path)}; `
-      : ''
-    stringAttributes += domain
-      ? `${nameof<CookieAttributes>(o => o.domain)}=${removeSemicolon(
-          domain,
-        )}; `
-      : ''
-    stringAttributes += isSecure
-      ? `${nameof<CookieAttributes>(o => o.secure)}; `
-      : ''
-    // cannot naming CookieAttributes.max-age
-    stringAttributes += maxAge ? `${'max-age' + '='}${maxAge}; ` : ''
-    stringAttributes += samesite
-      ? `${nameof<CookieAttributes>(o => o.samesite)}=${samesite}; `
-      : ''
-  }
+  if (attributes !== undefined)
+    stringAttributes = createStringAttribute(attributes)
 
   return (document.cookie = `${key}=${value}${stringAttributes}`)
 }
